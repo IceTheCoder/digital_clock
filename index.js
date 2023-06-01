7466733386bf17687edd3e90d400cebba95007f5
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-06-01 09:57:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
- const hour = document.getElementById("hour")
- const minutes = document.getElementById("minutes")
- const seconds = document.getElementById("seconds")
- const ampm = document.getElementById("ampm")
+const hour = document.getElementById("hour")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const ampm = document.getElementById("ampm")
 
 function updateClock()  {
-    let  h = new Date().getHours()
-    let  m = new Date().getMinutes()
-    let  s = new Date().getSeconds()
-    let ampm = "AM"
-    
-    if (h > 12 && h < 23) {
-        ampm = "PM"
-    }
-    
-    h = h < 10? "0" + h: h;
-    m = m < 10? "0" + m: m;
-    s = s < 10? "0" + s: s;
-    hour.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
+  let  h = new Date().getHours()
+  let  m = new Date().getMinutes()
+  let  s = new Date().getSeconds()
+  let ampm = "AM"
+  
+  if (h > 12 && h < 23) {
+      ampm = "PM"
+  }
+  
+  h = h < 10? "0" + h: h;
+  m = m < 10? "0" + m: m;
+  s = s < 10? "0" + s: s;
+  hour.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
 
-    setTimeout(() => {
-        updateClock()
-    }, 1000)
+  setTimeout(() => {
+      updateClock()
+  }, 1000)
 
 }
 
